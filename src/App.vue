62dad85a9e005841c9fa6cad59be77a14a9ce7a8
vagrant <vagrant@homestead>
2018-07-09 22:31:30 +0000

    Create stations component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
     <div class="app">
         <h1>VueRadio</h1>
+        <stations :stations="stations" @selectStation="onSelectStation"></stations>
     </div>
 </template>
 
 <script>
+    import Stations from './components/Stations.vue'
+
     export default {
-        name: 'app'
+        name: 'app',
+        data: function() {
+            return {
+                stations: [
+                    {
+                        name: 'rock',
+                        title: 'Rock & Roll'
+                    }
+                ]
+            }
+        },
+        methods: {
+            onSelectStation: function(station) {
+                console.log('selected station', station)
+            }
+        },
+        components: {
+            Stations
+        }
     }
+    
 </script>
---
+++ src/components/Stations.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="stations">
+        <h1>Stations</h1>
+        <ul>
+            <li v-for="station in stations" :key="station.name">
+                <a href="#"
+                    @click.prevent="selectStation(station)">{{ station.title }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'stations',
+        props: {
+            stations: {
+                type: Array,
+                required: true
+            }
+        },
+        methods: {
+            selectStation: function(station) {
+                this.$emit('selectStation', station)
+            }
+        }
+    }
+</script>
