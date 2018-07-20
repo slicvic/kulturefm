832f0d3af912decd16525fb742f2d488b9f0903b
vagrant <vagrant@homestead>
2018-07-20 23:49:15 +0000

    Indent

--- src/services/soundcloud.js
+++ src/services/soundcloud.js
@@ -7,11 +7,11 @@ SC.initialize({
 export default {
     getTracksByGenre(genre, successCallback, failureCallback) {
         return SC.get('/tracks', {
-            genres: genre,
-            limit: 100
-        })
-        .then(tracks => successCallback(tracks))
-        .catch(failureCallback)
+                genres: genre,
+                limit: 2
+            })
+            .then(tracks => successCallback(tracks.filter(t => t.streamable == true)))
+            .catch(failureCallback)
     },
     streamTrack(trackId) {
         return SC.stream('/tracks/' + trackId);
