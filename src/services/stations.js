761549902baee89da4a6815f0b725e2084233f7f
vagrant <vagrant@homestead>
2018-07-22 13:26:40 +0000

    Refactor stations service

--- src/services/stations.js
+++ src/services/stations.js
@@ -1,21 +1,72 @@
-import genreSvc from './genres.js'
-
 const StationType = {
     GENRE: 'genre'
 }
 
-const stations = []
-
-genreSvc.getGenres().forEach(genre => {
-    stations.push({
-        type: StationType.GENRE, 
-        name: genre.name, 
-        title: genre.title
-    })
-})
+const stations = [
+    {
+        name: 'Hip-Hop/Rap/Trap',
+        keywords: ['hip-hop', 'hip hop', 'hiphop', 'rap', 'trap']
+    },
+    {
+        name: 'Country',
+        keywords: ['country', 'country music']
+    },
+    {
+        name: 'Dance',
+        keywords: ['dance']
+    },
+    {
+        name: 'Electronic',
+        keywords: ['electronic', 'edm']
+    },
+    {
+        name: 'Indie',
+        keywords: ['indie', 'indie rock']
+    },
+    {
+        name: 'Jazz',
+        keywords: ['jazz']
+    },
+    {
+        name: 'K-Pop',
+        keywords: ['k-pop']
+    },
+    {
+        name: 'Latino',
+        keywords: ['latino', 'salsa', 'merengue', 'bachata', 'reggaeton'],
+    },
+    {
+        name: 'R&B',
+        keywords: ['r&b', 'rhythm and blues']
+    },
+    {
+        name: 'Pop',
+        keywords: ['pop']
+    },
+    {
+        name: 'Rock',
+        keywords: ['rock']
+    },
+    {
+        name: 'Reggae',
+        keywords: ['reggae']
+    }
+]
 
 export default {
     getStations() {
-        return stations
+        return stations.sort((a, b) => {
+            const nameOfA = a.name.toUpperCase()
+            const nameOfB = b.name.toUpperCase()
+
+            if (nameOfA < nameOfB) {
+                return -1
+            }
+            if (nameOfA > nameOfB) {
+                return 1
+            }
+
+            return 0
+        })
     }
 }
