12cef45447fabb6f4d084ef913fe84c0ac68fa79
vagrant <vagrant@homestead>
2018-07-22 13:27:14 +0000

    Refactor soundcloud service

--- src/services/soundcloud.js
+++ src/services/soundcloud.js
@@ -5,15 +5,34 @@ SC.initialize({
 })
 
 export default {
-    getTracksByGenre(genre, successCallback, failureCallback) {
-        return SC.get('/tracks', {
-                genres: genre,
-                limit: 2
-            })
+    streamTrack(trackId) {
+        return SC.stream('/tracks/' + trackId)
+    },
+    findTracks({
+        q = null, 
+        tags = null, 
+        genres = null, 
+        successCallback = () => {}, 
+        failureCallback = () => {}
+    }) {
+        const params = {
+            limit: 200
+        }
+
+        if (q) {
+            params.q = q
+        }
+
+        if (tags) {
+            params.tags = tags
+        }
+
+        if (genres) {
+            params.genres = genres
+        }
+
+        return SC.get('/tracks', params)
             .then(tracks => successCallback(tracks.filter(t => t.streamable == true)))
             .catch(failureCallback)
-    },
-    streamTrack(trackId) {
-        return SC.stream('/tracks/' + trackId);
     }
 }
